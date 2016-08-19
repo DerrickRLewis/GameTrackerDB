46241daf4e4647bc35dec7e164b390db1aae2f35
Derrick <derrickrlewis@gmail.com>
2016-08-19 12:04:33 -0500

    Redo

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,33 +1,54 @@
 package com.theironyard;
 
+import org.h2.tools.Server;
 import spark.ModelAndView;
 import spark.Session;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
-
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.HashMap;
+
+
+
+
+
+
 
 public class Main {
 
     static HashMap<String, User> users = new HashMap<>();
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws SQLException {
+
+        Server.createWebServer().start();
+        Connection conn = DriverManager.getConnection("jdbc:h2:./main");
+        Statement stmt = conn.createStatement();
+        stmt.execute("CREATE TABLE IF NOT EXISTS games (id IDENTITY, gameName VARCHAR, genre VARCHAR, platform VARCHAR, release_year INT)");
+
+
         Spark.init();
+
         Spark.get(
                 "/",
                 ((request, response) -> {
                     User user = getUserFromSession(request.session());
 
-                    HashMap m = new HashMap<>();
+                    HashMap<Object, Object> m = new HashMap<>();
                     if (user == null) {
                         return new ModelAndView(m, "login.html");
                     }
                     else {
-                        return new ModelAndView(user, "home.html");
+
+                        ArrayList<Game> gameArrayList = selectGames(conn);
+                        m.put("games", gameArrayList);
+                        return new ModelAndView(m, "home.html");
                     }
                 }),
                 new MustacheTemplateEngine()
         );
+
         Spark.post(
                 "/create-user",
                 ((request, response) -> {
@@ -45,26 +66,55 @@ public class Main {
                     return "";
                 })
         );
+
         Spark.post(
                 "/create-game",
                 ((request, response) -> {
                     User user = getUserFromSession(request.session());
-                    if (user == null) {
-                        //throw new Exception("User is not logged in");
-                        Spark.halt(403);
-                    }
 
                     String gameName = request.queryParams("gameName");
                     String gameGenre = request.queryParams("gameGenre");
                     String gamePlatform = request.queryParams("gamePlatform");
-                    int gameYear = Integer.valueOf(request.queryParams("gameYear"));
-                    Game game = new Game(gameName, gameGenre, gamePlatform, gameYear);
+                    Integer gameYear = Integer.valueOf(request.queryParams("gameYear"));
 
-                    user.games.add(game);
+                    Game game = new Game(gameName, gameGenre, gamePlatform, gameYear);
+                    insertGame(conn, gameName, gameGenre, gamePlatform, gameYear);
+
+
+                    if (user != null) {
+                        user.games.add(game);
+                    }
 
                     response.redirect("/");
                     return "";
                 })
+        );
+
+        Spark.post(
+                "/delete-game",
+                (request, response) -> {
+                    Integer id = Integer.parseInt(request.queryParams("id"));
+                    deleteGame(conn, id);
+
+                    response.redirect("/");
+                    return "";
+                }
+        );
+
+        Spark.post(
+                "/edit-game",
+                (request, response) -> {
+                    Integer id = Integer.parseInt(request.queryParams("ID"));
+                    String name = request.queryParams("gameName");
+                    String genre = request.queryParams("gameGenre");
+                    String platform = request.queryParams("gamePlatform");
+                    Integer gameYear = Integer.parseInt(request.queryParams("gameYear"));
+
+                    updateGame(conn, id, name, genre, platform, gameYear);
+
+                    response.redirect("/");
+                    return "";
+                }
         );
         Spark.post(
                 "/logout",
@@ -77,8 +127,65 @@ public class Main {
         );
     }
 
+    static void updateGame(Connection conn, Integer id, String name, String genre, String platform, Integer releaseYear) throws SQLException {
+
+        Statement stmt = conn.createStatement();
+        stmt.execute("UPDATE GAMES SET GAMENAME = '" + name + "' WHERE ID = " + id);
+        stmt.execute("UPDATE GAMES SET GENRE = '" + genre + "' WHERE ID = " + id);
+        stmt.execute("UPDATE GAMES SET PLATFORM = '" + platform + "' WHERE ID = " + id);
+        stmt.execute("UPDATE GAMES SET RELEASE_YEAR = " + releaseYear + " WHERE ID = " + id);
+    }
+
     static User getUserFromSession(Session session) {
         String name = session.attribute("userName");
         return users.get(name);
+    }
+
+    static ArrayList<Game> selectGames(Connection conn) throws SQLException {
+        ArrayList<Game> games = new ArrayList<>();
+        Statement stmt = conn.createStatement();
+        ResultSet results = stmt.executeQuery("SELECT * FROM GAMES");
+        while(results.next()){
+            Integer id = results.getInt("Id");
+            String name = results.getString("GameName");
+            String genre = results.getString("Genre");
+            String platform = results.getString("Platform");
+            Integer releaseYear = results.getInt("ReleaseYear");
+//Cant get this line working!!!!!!
+
+            games.add(new Game(id, name, genre, platform, releaseYear));
+//            selectGames().add(new Game((id, name, genre, platform, releaseYear));
+            stmt.execute("INSERT INTO games VALUES (NULL, name, genre, platform, releaseYear)");
+        }
+
+        return games;
+    }
+//    public static ArrayList<ToDoItem> selectToDos(Connection conn) throws SQLException {
+//        ArrayList<ToDoItem> items = new ArrayList<>();
+//        Statement stmt = conn.createStatement();
+//        ResultSet results = stmt.executeQuery("SELECT * FROM todos");
+//        while (results.next()) {
+//            int id = results.getInt("id");
+//            String text = results.getString("text");
+//            boolean isDone = results.getBoolean("is_done");
+//            items.add(new ToDoItem(id, text, isDone));
+//        }
+//        return items;
+//    }
+//
+
+
+    static void insertGame(Connection conn, String name, String genre, String platform, Integer releaseYear) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("INSERT INTO games VALUES (NULL, ?, ?, ?, ?)");
+        stmt.setString(1, name);
+        stmt.setString(2, genre);
+        stmt.setString(3, platform);
+        stmt.setInt(4, releaseYear);
+        stmt.execute();
+    }
+
+    static void deleteGame(Connection conn, Integer id) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("DELETE FROM GAMES WHERE ID = " + id);
+        stmt.execute();
     }
 }
